ea24e3ad6f2193b05ce6a87e00fab4c75a81f717
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-04 20:53:39 +0000

    Deploy Bob-MusicPlayer/front-bob to github.com/Bob-MusicPlayer/front-bob.git:gh-pages

---
+++ precache-manifest.61048deba1526e15b52d74b2fab48ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d265aa1ec31c6b8401708da5969d6b",
+    "url": "https://bob-musicplayer.github.io/front-bob/index.html"
+  },
+  {
+    "revision": "c98a5413bfaaabeac576",
+    "url": "https://bob-musicplayer.github.io/front-bob/static/css/main.f6296f07.chunk.css"
+  },
+  {
+    "revision": "168f4418e21472f42289",
+    "url": "https://bob-musicplayer.github.io/front-bob/static/js/2.92c8a8ff.chunk.js"
+  },
+  {
+    "revision": "c98a5413bfaaabeac576",
+    "url": "https://bob-musicplayer.github.io/front-bob/static/js/main.c017dc56.chunk.js"
+  },
+  {
+    "revision": "74ce35477705da0371eb",
+    "url": "https://bob-musicplayer.github.io/front-bob/static/js/runtime~main.5dc0a981.js"
+  }
+]);
